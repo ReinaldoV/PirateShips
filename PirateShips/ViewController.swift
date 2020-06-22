bda7b9ba989e664a1a27c05bc386eb96050d44ec
Reinaldo Villanueva <reinaldovillajavi@gmail.com>
2020-06-22 13:13:22 +0200

    Setup of Project

---
+++ PirateShips/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PirateShips
+//
+//  Created by Reinaldo Villanueva Javierre on 22/06/2020.
+//  Copyright © 2020 Reinaldo Villanueva Javierre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
