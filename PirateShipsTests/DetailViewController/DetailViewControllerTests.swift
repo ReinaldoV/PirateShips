4b2b3c6ccd80e49961e09e04acb667934c5574a6
Reinaldo Villanueva <reinaldovillajavi@gmail.com>
2020-06-24 15:25:08 +0200

    Added tests to the detail viewcontroller and presenter

---
+++ PirateShipsTests/DetailViewController/DetailViewControllerTests.swift
@@ -0,0 +1,44 @@
+//
+//  DetailViewControllerTests.swift
+//  PirateShipsTests
+//
+//  Created by Reinaldo Villanueva Javierre on 24/06/2020.
+//  Copyright © 2020 Reinaldo Villanueva Javierre. All rights reserved.
+//
+
+import XCTest
+@testable import PirateShips
+
+class DetailViewControllerTests: XCTestCase {
+
+    var sut: DetailViewController!
+
+    override func setUp() {
+        super.setUp()
+        createSut()
+    }
+
+    override func tearDown() {
+        releaseSut()
+        super.tearDown()
+    }
+
+    func createSut() {
+        sut = DetailViewController()
+    }
+
+    func releaseSut() {
+        sut = nil
+    }
+
+    // MARK: - Basic test.
+    func testSutIsntNil() {
+        XCTAssertNotNil(sut, "Sut must not be nil.")
+    }
+
+    // MARK: - Test viewDidLoad
+    func testLoadViewSuccess() {
+        _ = sut.view
+        sut.viewWillAppear(true)
+    }
+}
